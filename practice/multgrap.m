3d0c870e2380a27d53811aecb31028c5ce81fc1b
Guru <111226290+GGurucharan@users.noreply.github.com>
2023-01-20 19:26:43 +0530

    Delete practice directory

--- practice/multgrap.m
+++
@@ -1,8 +0,0 @@
-clc 
-clear all
-close all
-x = 0:0.1:2*pi;
-subplot(2,1,1)
-plot(x,cos(x),'r*')
-subplot(2,1,2)
-plot(x,sin(x));
--- practice/multplot.m
+++
@@ -1,9 +0,0 @@
-clc 
-clear 
-close all
-
-x = linspace(0,1)
-plot(x,x.^2,'r*')
-hold on
-plot(x,sin(x),'g.')
-plot(x,exp(x),'m+')
